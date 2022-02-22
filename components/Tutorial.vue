120396a01f8bf2ff705e252184bf3102e3b6418a
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2022-02-23 00:09:22 +0100

    Init

---
+++ components/Tutorial.vue
@@ -0,0 +1,26 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <button @click="onClick">Click me</button>
+  </div>
+</template>
+
+<script>
+
+import { JsonServiceClient } from "@servicestack/client"
+
+
+export default {
+  name: 'NuxtTutorial',
+  methods: {
+    onClick: function() {
+      console.log('hi');
+
+      let json = new JsonServiceClient();
+
+
+    }
+  }
+
+}
+</script>
